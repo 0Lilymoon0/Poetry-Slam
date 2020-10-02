c5b8d22b333a8e8795431b98fac16ef5c2932fcf
0Lilymoon0 <thelilcinnabun1@gmail.com>
2020-10-01 18:05:11 -0700

    Fourth Edit

--- poem.py
+++ poem.py
@@ -22,3 +22,16 @@ infile = open("poem.txt", "r")
 lines_printed_backwards(infile.read().splitlines())
 infile.close
 print("\n")
+
+def lines_printed_random(filename):
+    num_lines = len(filename)
+    for i in range(num_lines):
+        random_select_line = random.randint(1, num_lines - 1)
+        print(filename[random_select_line])
+    return []
+
+infile = open("poem.txt", "r")
+poemList = infile.read().splitlines()
+lines_printed_random(poemList)
+infile.close
+print("\n")
